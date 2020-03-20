ded5887df5693e0399b870a2629f0f1729c23727
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-20 11:13:13 +0300

    add class EquilateralImageView

--- app/src/main/java/com/bankmtk/pictureoftheday/MainActivity.kt
+++ app/src/main/java/com/bankmtk/pictureoftheday/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.bankmtk.pictureoftheday
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
 import com.bankmtk.pictureoftheday.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,14 @@ class MainActivity : AppCompatActivity() {
                     .commitNow()
         }
     }
-
+}
+class EquilateralImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet?=null,
+    defStyle: Int = 0
+): AppCompatImageView(context,attrs,defStyle){
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
 }
 
